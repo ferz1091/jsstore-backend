1f7d469f07feeaa78cb4a9934bd19c0defb1a46f
ferz1091 <ferz1091@gmail.com>
2023-04-06 21:34:27 +0300

    init vercel build

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ credentials: true, origin: 'http://localhost:5173'}));
+app.use(cors({ credentials: true, origin: 'https://jsstore-frontend.vercel.app'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/auth', authRouter);
